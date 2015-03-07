ebf2a19d85dfa4e88daf823ec74505e15cfaf1dd
David G Chaves <davidgchaves@gmail.com>
2015-03-07 11:07:24 +0100

    Add Ramda's split definition as a working spec
    split
      definition
        splits a string into an array of strings based on the given separator

--- test/01_currying_specs.js
+++ test/01_currying_specs.js
@@ -5,3 +5,16 @@ var R = require('ramda');
 var chai = require('chai'),
     expect = chai.expect;
 
+/*
+ * split :: String -> String -> [String]
+ */
+describe("Ramda's split", function () {
+
+  context('definition', function () {
+    it('splits a string into an array of strings based on the given separator', function () {
+      expect(R.split('/', 'usr/local/bin/node')).to.be.deep.equal(['usr', 'local', 'bin', 'node']);
+    });
+  });
+
+});
+
