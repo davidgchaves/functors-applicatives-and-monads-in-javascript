caab818571481d839b6594d3dfdbf624bbc92a9c
David G Chaves <davidgchaves@gmail.com>
2015-03-09 13:13:40 +0100

    Add 'articles' data structure

--- test/02_compose_specs.js
+++ test/02_compose_specs.js
@@ -88,3 +88,26 @@ describe("Ramda's prop", function () {
 
 });
 
+
+/*
+ * Playing with the articles data structure
+ */
+var articles = [
+  {
+    title: 'Everything Sucks',
+    url: 'http://do.wn/sucks.html',
+    author: {
+      name: 'Debbie Downer',
+      email: 'debbie@do.wn'
+    }
+  },
+  {
+    title: 'If You Please',
+    url: 'http://www.geocities.com/milq',
+    author: {
+      name: 'Caspar Milquetoast',
+      email: 'hello@me.com'
+    }
+  }
+];
+
