81e83769543da081aab93d698f50ec0c18d5b9f6
David G Chaves <davidgchaves@gmail.com>
2015-03-09 11:05:25 +0100

    Add Ramda's compose definition as a working spec
    Ramda's compose
      definition
        composes the given functions from right to left

--- test/02_compose_specs.js
+++ test/02_compose_specs.js
@@ -6,3 +6,21 @@ var chai = require('chai'),
     expect = chai.expect;
 
 
+/*
+ * compose :: ((y -> z) -> (x -> y) -> ... -> (b -> c) -> (a -> b)) -> (a -> z)
+ */
+describe("Ramda's compose", function () {
+
+  context('definition', function () {
+    it('composes the given functions from right to left', function () {
+      var triple = function(x) { return 3*x; };
+      var minus1 = function(x) { return x-1; };
+      var square = function(x) { return x*x; };
+
+      expect(R.compose(triple, minus1, square)(10)).to.be.equal(297);
+      expect(R.compose(square, minus1, triple)(10)).to.be.equal(841);
+    });
+  });
+
+});
+
