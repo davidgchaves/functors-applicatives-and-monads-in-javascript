09bf0dea42a05ae08aa7633a180258be8709a07a
David G Chaves <davidgchaves@gmail.com>
2015-03-10 16:24:31 +0100

    Add 'Just enough Category Theory' notes

--- test/03_functors_specs.js
+++ test/03_functors_specs.js
@@ -1,3 +1,25 @@
+/*
+ * JUST ENOUGH CATEGORY THEORY:
+ *
+ *  1 - FORMING A CATEGORY:
+ *
+ *    You need a composition function and an identity function to form a CATEGORY:
+ *      compose :: (b -> c) -> (a -> b) -> (a -> c)
+ *      id      :: a -> a
+ *
+ *  2 - CATEGORY LAWS:
+ *
+ *    LEFT IDENTITY:  compose(id,f)            == f
+ *    RIGHT IDENTITY: compose(f,id)            == f
+ *    ASSOCIATIVITY:  compose(compose(f,g), h) == compose(f, compose(g,h))
+ *
+ *  3 - CATEGORY LAWS USING HASKELL NOTATION:
+ *
+ *    LEFT IDENTITY:  id . f      = f
+ *    RIGHT IDENTITY: f . id      = f
+ *    ASSOCIATIVITY:  (f . g) . h = f . (g . h)
+ */
+
 'use strict';
 
 var R = require('ramda');
