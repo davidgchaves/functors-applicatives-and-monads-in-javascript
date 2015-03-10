c73432c9961ce4a1917c5138b9309a1675124ecb
David G Chaves <davidgchaves@gmail.com>
2015-03-10 16:31:35 +0100

    Add an Identity Functor implementation

--- test/03_functors_specs.js
+++ test/03_functors_specs.js
@@ -59,3 +59,19 @@ var fmap = R.curry(function(f,obj) {
   return obj.fmap(f);
 });
 
+
+/*
+ * The Identity Functor: A simple wrapper around a value
+ */
+
+// The actual Identity object
+var _Identity = function(a) { this.val = a; };
+
+// The Identity constructor
+var  Identity = function(a) { return new _Identity(a); };
+
+// The Identity fmap implementation
+_Identity.prototype.fmap = function(f) {
+  return new _Identity(f(this.val));
+};
+
