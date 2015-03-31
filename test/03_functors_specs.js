73db817c1ead7f61f9136f42295c7790684f22bb
David G Chaves <davidgchaves@gmail.com>
2015-03-31 20:54:11 +0200

    Extract IO Functor implementation from spec file

--- test/03_functors_specs.js
+++ test/03_functors_specs.js
@@ -43,7 +43,8 @@ var chai = require('chai'),
     expect = chai.expect;
 
 var Identity = require('../lib/identity_functor'),
-    Maybe    = require('../lib/maybe_functor');
+    Maybe    = require('../lib/maybe_functor'),
+    IO       = require('../lib/io_functor');
 
 /*
  * A POINT-FREE FMAP:
@@ -182,69 +183,10 @@ var eitherSaveUser = R.compose(fmap(mockSave), eitherUserNameIsLargerThan3CharsV
 
 /*
  * The IO Functor: Builds a lazy computation (a function) that you can run anytime with runIO()
- *
- *  Typically used to contain side effects
- *
- *  In this case, fmap appends the computation to a list (chain) of computations to run
- *
- *  Canibalizing the implementation from PointFree-Fantasy
  */
 
 // Compute now!
-var runIO = function(io) {
-  return io.val.apply(this, [].slice.call(arguments, 1));
-};
-
-// The actual IO object
-var _IO = function(fn) {
-  this.val = fn;
-  this.runIO = this.val;
-};
-
-// The IO constructor
-var IO = function(fn) {
-  return new _IO(fn);
-};
-
-_IO.of = function(x) {
-  return IO(function() {
-    return x;
-  });
-};
-_IO.prototype.of = _IO.of;
-
-// The list (chain) of computations fmap appends to
-_IO.prototype.chain = function(g) {
-  var io = this;
-  return IO(function() {
-    return g(io.val()).val();
-  });
-};
-
-// The IO fmap implementation
-_IO.prototype.fmap = function(f) {
-  return this.chain(function(a) {
-    return _IO.of(f(a));
-  });
-};
-
-// Monkey patching every JS function to support .toIO() in order to
-// be able to easily pass arguments to the function inside the IO Functor:
-//
-//  USING THE IO CONSTRUCTOR (No args)
-//    var email_io = IO(function () { return $(#email).val(); });
-//
-//  USING .toIO() (With args)
-//    var get_value_io = function(selector) { return $(selector).val(); }.toIO();
-//
-// Every time you monkey patch, God kills a kitten
-Function.prototype.toIO = function() {
-  var self = this;
-  return function(x) {
-    var args = arguments;
-    return IO(function() { return self.apply(this, args) });
-  };
-};
+var runIO = function(io) { return io.val.apply(this, [].slice.call(arguments, 1)); };
 
 describe('IO Functor', function () {
 
