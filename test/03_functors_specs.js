b4de5ed448c1b1a96adab9417915a287f4bf8297
David G Chaves <davidgchaves@gmail.com>
2015-03-31 20:41:27 +0200

    Extract Maybe Functor implementation from spec file

--- test/03_functors_specs.js
+++ test/03_functors_specs.js
@@ -42,7 +42,8 @@ var R = require('ramda');
 var chai = require('chai'),
     expect = chai.expect;
 
-var Identity = require('../lib/identity_functor');
+var Identity = require('../lib/identity_functor'),
+    Maybe    = require('../lib/maybe_functor');
 
 /*
  * A POINT-FREE FMAP:
@@ -89,18 +90,6 @@ var mapHead = fmap(R.head);
 /*
  * The Maybe Functor: Captures a NULL check (the value inside MAY or MAY NOT be there)
  */
-
-// The actual Maybe object:
-var _Maybe = function(a) { this.val = a; };
-
-// The Maybe constructor:
-var  Maybe = function(a) { return new _Maybe(a); };
-
-// The Maybe fmap implementation
-_Maybe.prototype.fmap = function(f) {
-  return this.val ? Maybe(f(this.val)) : Maybe(null);
-};
-
 describe('Maybe Functor', function () {
 
   describe('maybeNameStartsWith', function () {
