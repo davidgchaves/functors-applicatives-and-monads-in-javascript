f807ec201b517329e93c7e2221fceb6ff6675ca8
David G Chaves <davidgchaves@gmail.com>
2015-04-01 21:28:23 +0200

    Split specs from implementation code for functor examples

--- test/03_functors_specs.js
+++ test/03_functors_specs.js
@@ -1,45 +1,9 @@
-/*
- * JUST ENOUGH CATEGORY THEORY:
- *
- *  1 - FORMING A CATEGORY:
- *
- *    You need a composition function and an identity function to form a CATEGORY:
- *      compose :: (b -> c) -> (a -> b) -> (a -> c)
- *      id      :: a -> a
- *
- *  2 - CATEGORY LAWS:
- *
- *    LEFT IDENTITY:  compose(id,f)            == f
- *    RIGHT IDENTITY: compose(f,id)            == f
- *    ASSOCIATIVITY:  compose(compose(f,g), h) == compose(f, compose(g,h))
- *
- *  3 - CATEGORY LAWS USING HASKELL NOTATION:
- *
- *    LEFT IDENTITY:  id . f      = f
- *    RIGHT IDENTITY: f . id      = f
- *    ASSOCIATIVITY:  (f . g) . h = f . (g . h)
- */
-
-/*
- * FUNCTORS:
- *
- *  Any object or data structure you can map over
- *
- *  MAP IS THE KEY:
- *    You CAN'T call a function over a Functor
- *    You HAVE TO MAP the function over the Functor (AKA LIFTING)
- *      we lift our function into the Functor
- *
- *  MAP:
- *    (1) Takes the value out of its context to apply the function over it (lifting the function)
- *    (2) Boxes the resulting value back up into it's original context
- */
-
 'use strict';
 
-var R = require('ramda');
+var R      = require('ramda'),
+    Either = require('data.either');
 
-var chai = require('chai'),
+var chai   = require('chai'),
     expect = chai.expect;
 
 var Identity = require('../lib/identity_functor'),
@@ -48,6 +12,18 @@ var Identity = require('../lib/identity_functor'),
     helpers  = require('../lib/functor_helpers'),
     fmap     = helpers.fmap,
     runIO    = helpers.runIO;
+
+var lib = require('../lib/03_functors'),
+    add1                                      = lib.add1,
+    mapHead                                   = lib.mapHead,
+    maybeNameStartsWith                       = lib.maybeNameStartsWith,
+    maybeParseInt                             = lib.maybeParseInt,
+    eitherCheckIfUserIsActive                 = lib.eitherCheckIfUserIsActive,
+    eitherGrantAccess                         = lib.eitherGrantAccess,
+    eitherUserNameIsLargerThan3CharsValidator = lib.eitherUserNameIsLargerThan3CharsValidator,
+    eitherSaveUser                            = lib.eitherSaveUser,
+    ioGetProtocol                             = lib.ioGetProtocol,
+    ioMaybeGetUserEmail                       = lib.ioMaybeGetUserEmail;
 
 /*
  * The Identity Functor: A simple wrapper around a value
@@ -68,9 +44,6 @@ describe('Identity Functor', function () {
   });
 
 });
-
-var add1    = fmap(R.add(1));
-var mapHead = fmap(R.head);
 
 
 /*
@@ -102,9 +75,6 @@ describe('Maybe Functor', function () {
 
 });
 
-var maybeNameStartsWith = R.compose(fmap(R.head), Maybe, R.prop('name'));
-var maybeParseInt       = R.compose(fmap(parseInt), Maybe);
-
 
 /*
  * The Either Functor: A structure for disjunctions (e.g.: computations that may fail)
@@ -114,56 +84,41 @@ var maybeParseInt       = R.compose(fmap(parseInt), Maybe);
  *  Using the Data.Either implementation: https://github.com/folktale/data.either
  *  from the Folktale project: http://folktalejs.org
  */
-var E = require('data.either');
-
 describe('Either Functor', function () {
 
   describe('eitherGrantAccess', function () {
     it('returns a welcome message in an Either.Right if user is active', function () {
       var david = { active: true, name: 'David' };
-      expect(eitherGrantAccess(david)).to.be.deep.equal(E.Right('Welcome David'));
+      expect(eitherGrantAccess(david)).to.be.deep.equal(Either.Right('Welcome David'));
     });
 
     it('returns an error message in an Either.Left if user is non-active', function () {
       var peter = { active: false, name: 'Peter' };
-      expect(eitherGrantAccess(peter)).to.be.deep.equal(E.Left('Your account is not active'));
+      expect(eitherGrantAccess(peter)).to.be.deep.equal(Either.Left('Your account is not active'));
     });
   });
 
   describe('eitherUserNameIsLargerThan3CharsValidator', function () {
     it("returns the given value in an Either.Right when it's length is greater than 3", function () {
-      expect(eitherUserNameIsLargerThan3CharsValidator('fprules64')).to.be.deep.equal(E.Right('fprules64'));
+      expect(eitherUserNameIsLargerThan3CharsValidator('fprules64')).to.be.deep.equal(Either.Right('fprules64'));
     });
 
     it('returns an error message in an Either.Left to non-active users', function () {
-      expect(eitherUserNameIsLargerThan3CharsValidator('123')).to.be.deep.equal(E.Left('Need length greater than 3'));
+      expect(eitherUserNameIsLargerThan3CharsValidator('123')).to.be.deep.equal(Either.Left('Need length greater than 3'));
     });
   });
 
   describe('eitherSaveUser (with side-effects)', function () {
     it("returns the saved user in an Either.Right", function () {
-      expect(eitherSaveUser('fprules64')).to.be.deep.equal(E.Right('fprules64'));
+      expect(eitherSaveUser('fprules64')).to.be.deep.equal(Either.Right('fprules64'));
     });
 
     it('returns an error message in an Either.Left to non-active users', function () {
-      expect(eitherSaveUser('123')).to.be.deep.equal(E.Left('Need length greater than 3'));
+      expect(eitherSaveUser('123')).to.be.deep.equal(Either.Left('Need length greater than 3'));
     });
   });
 
 });
-
-var eitherCheckIfUserIsActive = function(user) {
-  return user.active ? E.Right(user) : E.Left('Your account is not active');
-};
-var welcomeUser = R.compose(R.add('Welcome '), R.prop('name'));
-var eitherGrantAccess = R.compose(fmap(welcomeUser), eitherCheckIfUserIsActive);
-
-var eitherUserNameIsLargerThan3CharsValidator = function(x) {
-  return x.length > 3 ? E.Right(x) : E.Left('Need length greater than 3');
-};
-
-var mockSave = function (x) { console.log('User ' + x + ' saved!'); return x; };
-var eitherSaveUser = R.compose(fmap(mockSave), eitherUserNameIsLargerThan3CharsValidator);
 
 
 /*
@@ -190,26 +145,4 @@ describe('IO Functor', function () {
   });
 
 });
-
-
-var ioGetHref = function () { return 'http://www.example.com'; }.toIO();
-var getProtocol = R.compose(R.head, R.split('/'));
-var ioGetProtocol = R.compose(fmap(getProtocol), ioGetHref);
-
-
-// Quite interesting:
-//  A Maybe Functor (maybeLocalStorageForUser) inside an IO Functor (ioMaybeGetUserInLocalStorage)
-//  Hence the double fmap (we need to go 2-functor-level-deep)
-
-// The Maybe Functor
-var maybeLocalStorageForUser = function(user) { return Maybe(JSON.stringify(user)) };
-
-// The IO Functor with a Maybe Functor inside
-var ioMaybeGetUserInLocalStorage = function(user) { return maybeLocalStorageForUser(user); }.toIO();
-
-// The function we want to map over
-var getUserEmail = R.compose(R.prop('email'), JSON.parse);
-
-// The double fmap
-var ioMaybeGetUserEmail = R.compose(fmap(fmap(getUserEmail)), ioMaybeGetUserInLocalStorage);
 
